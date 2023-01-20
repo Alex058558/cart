8e9ad996f6199151110c6f2de76f88778ff548be
Alex Ting <alexting058558@gmail.com>
2023-01-20 18:47:06 +0800

    using command commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule} from '@angular/router'
+import { ReactiveFormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      {path:'',component:ProductListComponent}
+    ])
+  ],
   declarations: [
     AppComponent,
     ProductListComponent,
     TopBarComponent
   ],
-  imports: [
-    BrowserModule
-  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { products,Product } from '../product';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+  products:Product[] = products 
+  constructor(){}
 
+  share(){
+    window.alert('The product has been shared!');
+  }
 }
