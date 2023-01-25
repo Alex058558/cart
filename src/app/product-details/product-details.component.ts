9e7a7333189f30443aec156f9f18f0cd773b246c
Alex Ting <alexting058558@gmail.com>
2023-01-25 17:47:23 +0800

    add cart component and template

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,19 +1,28 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product, products } from '../product';
+import { CartService } from '../cart.service';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent {
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+      private route: ActivatedRoute,
+      private cartService: CartService
+      ) { }
   product: Product|undefined;
 
   ngOnInit() {
-    const routeParams = this.route.snapshot.paramMap;
-    const productIdFromRoute = Number(routeParams.get('productId'));
-    console.log(productIdFromRoute);
-    this.product = products.find(product => product.id == productIdFromRoute);
+    // const routeParams = this.route.snapshot.paramMap;
+    // const productIdFromRoute = Number(routeParams.get('productId'));
+    // console.log(productIdFromRoute);
+    this.product = products.find(product => product.id == this.route.snapshot.params['productId']);
+  }
+
+  addToCart(product: Product){
+    this.cartService.addToCart(product);
+    window.alert(`Your product has been added to the cart`);
   }
 }
