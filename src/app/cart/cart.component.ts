f3b92f5cbda1f3674f6163aee9977ed19213c625
Alex Ting <alexting058558@gmail.com>
2023-02-01 10:37:30 +0800

    Add the user input and form validate

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
-  constructor(private cartService: CartService) { }
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
   items = this.cartService.getItems();
+  checkoutForm = this.formBuilder.group({
+    name: ['',Validators.required],
+    address: ['',Validators.required],
+  });
+
+  onSubmit() {
+    if(this.checkoutForm.status == 'VALID'){
+      this.items = this.cartService.clearCart();
+      this.checkoutForm.reset();
+    }else{
+      alert('Please checkout the input!');
+    }
+  }
 }
