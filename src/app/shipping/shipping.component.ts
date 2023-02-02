3369cdbb54df1aa83fe1e043ef38479788590e7e
Alex Ting <alexting058558@gmail.com>
2023-02-02 09:03:47 +0800

    add the ngonit implements

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CartService } from '../cart.service';
 
@@ -7,7 +7,7 @@ import { CartService } from '../cart.service';
   templateUrl: './shipping.component.html',
   styleUrls: ['./shipping.component.css']
 })
-export class ShippingComponent {
+export class ShippingComponent implements OnInit {
   constructor(private cartService: CartService) { }
   shippingCosts!:Observable<{type:string,price:number}[]>;
   ngOnInit():void { 
